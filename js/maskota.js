87bf50bdb724872a9d46f28371f038827c986215
Rodrigo Luis Santander <roidersp@gmail.com>
2016-04-19 14:24:29 -0500

    dfgdgd

--- js/maskota.js
+++ js/maskota.js
@@ -1,4 +1,6 @@
 $(function() {
+	calcular_alto();
+	
     $(document).on("click",".tips_icon",function(){
 	   var num = $(this).attr("num");
 	   $(".tips_info_content").fadeOut().removeClass("active");
@@ -7,4 +9,36 @@ $(function() {
 	   $(this).addClass("active");
 	   	    
     });
+    
+  /* minigrid({
+	  container: '#adopta_content',
+	  item: '.adopta_item',
+	  gutter: 0
+	});
+	
+	window.addEventListener('resize', function(){
+		  minigrid({
+		    container: '#adopta_content',
+		    item: '.adopta_item',
+		    gutter: 0
+		  });
+		});*/
+    
 });
+
+$( window ).resize(function() {
+	console.log("resize");
+	$(".row-eq-height").height("auto");
+  calcular_alto();
+});
+
+
+function calcular_alto(){
+	 var heights = $(".row-eq-height").map(function() {
+        return $(this).height();
+    }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+
+    $(".row-eq-height").height(maxHeight);
+}
